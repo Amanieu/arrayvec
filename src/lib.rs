9c9dfcaffc4d912169fee77ca5a5129be201398e
bluss <bluss@users.noreply.github.com>
2018-03-25 22:28:33 +0200

    FEAT: Implement a careful “MaybeUninit” using ManuallyDrop
    This is a cautious version of MaybeUninit, since we don't have one in
    libstd, based on ManuallyDrop.
    
    This moves ArrayVec to a nice, no size overhead implementation by
    default. We use Rust version sniffing (build script) to automatically
    use this for new enough Rust versions.
    
    This doesn't kill nodrop unfortunately, it still remains as the
    fallback.

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,6 @@
 //!   - Optional, enabled by default
 //!   - Use libstd; disable to use `no_std` instead.
 //!
-//! - `use_union`
-//!   - Optional
-//!   - Requires Rust nightly channel
-//!   - Experimental: This flag uses nightly so it *may break* unexpectedly
-//!     at some point; since it doesn't change API this flag may also change
-//!     to do nothing in the future.
-//!   - Use the unstable feature untagged unions for the internal implementation,
-//!     which may have reduced space overhead
 //! - `serde-1`
 //!   - Optional
 //!   - Enable serialization for ArrayVec and ArrayString using serde 1.0
@@ -25,6 +17,7 @@
 //!
 #![doc(html_root_url="https://docs.rs/arrayvec/0.4/")]
 #![cfg_attr(not(feature="std"), no_std)]
+
 extern crate nodrop;
 #[cfg(feature="serde-1")]
 extern crate serde;
@@ -50,11 +43,16 @@ use std::fmt;
 #[cfg(feature="std")]
 use std::io;
 
-#[cfg(not(feature="use_union"))]
-use nodrop::NoDrop;
 
-#[cfg(feature="use_union")]
-use std::mem::ManuallyDrop as NoDrop;
+#[cfg(has_manuallydrop)]
+mod maybe_uninit;
+#[cfg(has_manuallydrop)]
+use maybe_uninit::MaybeUninit;
+
+#[cfg(not(has_manuallydrop))]
+mod maybe_uninit_nodrop;
+#[cfg(not(has_manuallydrop))]
+use maybe_uninit_nodrop::MaybeUninit;
 
 #[cfg(feature="serde-1")]
 use serde::{Serialize, Deserialize, Serializer, Deserializer};
@@ -93,7 +91,7 @@ unsafe fn new_array<A: Array>() -> A {
 ///
 /// ArrayVec can be converted into a by value iterator.
 pub struct ArrayVec<A: Array> {
-    xs: NoDrop<A>,
+    xs: MaybeUninit<A>,
     len: A::Index,
 }
 
@@ -130,7 +128,7 @@ impl<A: Array> ArrayVec<A> {
     /// ```
     pub fn new() -> ArrayVec<A> {
         unsafe {
-            ArrayVec { xs: NoDrop::new(new_array()), len: Index::from(0) }
+            ArrayVec { xs: MaybeUninit::uninitialized(), len: Index::from(0) }
         }
     }
 
@@ -574,7 +572,7 @@ impl<A: Array> ArrayVec<A> {
             Err(self)
         } else {
             unsafe {
-                let array = ptr::read(&*self.xs);
+                let array = ptr::read(self.xs.ptr());
                 mem::forget(self);
                 Ok(array)
             }
@@ -603,7 +601,7 @@ impl<A: Array> Deref for ArrayVec<A> {
     #[inline]
     fn deref(&self) -> &[A::Item] {
         unsafe {
-            slice::from_raw_parts(self.xs.as_ptr(), self.len())
+            slice::from_raw_parts((*self.xs.ptr()).as_ptr(), self.len())
         }
     }
 }
@@ -613,7 +611,7 @@ impl<A: Array> DerefMut for ArrayVec<A> {
     fn deref_mut(&mut self) -> &mut [A::Item] {
         let len = self.len();
         unsafe {
-            slice::from_raw_parts_mut(self.xs.as_mut_ptr(), len)
+            slice::from_raw_parts_mut((*self.xs.ptr_mut()).as_mut_ptr(), len)
         }
     }
 }
@@ -629,7 +627,7 @@ impl<A: Array> DerefMut for ArrayVec<A> {
 /// ```
 impl<A: Array> From<A> for ArrayVec<A> {
     fn from(array: A) -> Self {
-        ArrayVec { xs: NoDrop::new(array), len: Index::from(A::capacity()) }
+        ArrayVec { xs: MaybeUninit::from(array), len: Index::from(A::capacity()) }
     }
 }
 
